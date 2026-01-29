3e79ef4aa7f1aaf46ba6c2ce987a08816a4192a0
Daniel TISCHER <dos.7182@gmail.com>
2026-01-29 22:10:26 +0100

    refactor

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"log"
-	"net"
-	"net/rpc"
 	"os"
 )
 
@@ -99,48 +96,5 @@ EXAMPLES:`)
 	}
 
 	fmt.Printf("%s %s starting...\n", name, version)
-
-	// Configure listener
-	address := fmt.Sprintf("%s:%d", cfg.address, cfg.port)
-	listener, err := net.Listen(cfg.network, address)
-	if err != nil {
-		log.Fatalf("Failed to listen on %s: %v", address, err)
-	}
-
-	// Configure and start ExecStateManager
-	manager := &ExecStateManager{listener: listener}
-	manager.Start()
-	defer manager.Stop()
-
-	// set the initial sleep mode
-	if cfg.display {
-		if err := manager.Display(ExecStateRequest{}, &ExecStateReply{}); err != nil {
-			log.Fatalf("Failed to set initial display state: %v", err)
-		}
-	} else {
-		if err := manager.System(ExecStateRequest{}, &ExecStateReply{}); err != nil {
-			log.Fatalf("Failed to set initial system state: %v", err)
-		}
-	}
-
-	// Register RPC server with ExecStateManager methods
-	if err := rpc.Register(manager); err != nil {
-		log.Fatalf("Failed to register RPC server: %v", err)
-	}
-
-	log.Printf("RPC server listening on %s (%s)", address, cfg.network)
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			// Exit cleanly if the listener was intentionally closed.
-			if errors.Is(err, net.ErrClosed) { // Go 1.16+
-				break
-			}
-			// Other errors are real and should be logged/handled.
-			log.Printf("accept error: %v", err)
-			continue
-		}
-		go rpc.ServeConn(conn)
-	}
-	log.Println("RPC server shutdown complete.")
+	serve(cfg)
 }
